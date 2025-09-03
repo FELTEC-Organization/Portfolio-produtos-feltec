675329de1ca3d3cc1e3cd115b4cd14ca49e3b255
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-09-03 17:41:21 +0000

    Adição dos novos serviços

--- src/data/products.ts
+++ src/data/products.ts
@@ -2,8 +2,7 @@ export type Product = {
   id: string;
   name: string;
   description: string;
-  price: number;
-  category: 'Portfolios' | 'Sites' | 'Apps' | 'Landing Pages' | 'IA';
+  category: 'Portfolios' | 'Sites' | 'Apps' | 'Landing Pages' | 'IA' | 'Ecommerce';
   tags: string[];
   inStock: boolean;
   image: string;
@@ -12,9 +11,8 @@ export type Product = {
 export const PRODUCTS: Product[] = [
   {
     id: 'p1',
-    name: 'Portfolio pessoal',
+    name: 'Portfolio',
     description: 'Landing page personalizada com a sua marca, ideal para profissionais autônomos e para currículo',
-    price: 499.90,
     category: 'Portfolios',
     tags: ['Portfolio', 'pessoal', 'profissional'],
     inStock: true,
@@ -24,7 +22,6 @@ export const PRODUCTS: Product[] = [
     id: 'p2',
     name: 'Websites',
     description: 'Website profissional para sua empresa, com catálogo de produtos',
-    price: 699.00,
     category: 'Sites',
     tags: ['Websites', 'Profissional', 'produtos'],
     inStock: true,
@@ -34,7 +31,6 @@ export const PRODUCTS: Product[] = [
     id: 'p3',
     name: 'Landing Pages',
     description: 'Landing page para comércios, catálogos, etc..',
-    price: 2599.00,
     category: 'Landing Pages',
     tags: ['Landing Pages', 'Profissional'],
     inStock: true,
@@ -44,12 +40,47 @@ export const PRODUCTS: Product[] = [
     id: 'p4',
     name: 'Chatbots',
     description: 'Chatbot feito especialmente para seu site',
-    price: 149.90,
     category: 'IA',
     tags: ['chatbot', 'automação', 'ia'],
     inStock: true,
     image: 'https://images.unsplash.com/photo-1718241905462-56e7b9f722f3?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' // ícone de IA/chatbot futurista
   },
+    {
+    id: 'p5',
+    name: 'E-commerce',
+    description: 'E-commerce pensado para seu negócio',
+    category: 'Ecommerce',
+    tags: ['Ecommerce', 'profissional', 'produto'],
+    inStock: true,
+    image: ''
+  },
+    {
+    id: 'p6',
+    name: 'Portfolio premium',
+    description: 'Portfolio aprimorado, com adicionais como: tema escuro, intenacionalização e carrousel de serviços',
+    category: 'IA',
+    tags: ['profissional', 'pessoal', 'Portfolio'],
+    inStock: true,
+    image: '' 
+  },
+    {
+    id: 'p7',
+    name: 'Portfolio com aba produtos',
+    description: 'Portfolio completo + aba separada para produtos ou serviços',
+    category: 'IA',
+    tags: ['profissional', 'Portfolio', 'Produtos'],
+    inStock: true,
+    image: ''
+  },
+    {
+    id: 'p8',
+    name: 'Aprimoramentos de site',
+    description: 'Mudanças no design, internacionalização, adição de funcionalidades, etc...',
+    category: 'Sites',
+    tags: ['Sites', 'suporte', 'aprimoramento'],
+    inStock: true,
+    image: '' 
+  },
 ];
 
-export const CATEGORIES = ['IA', 'Landing Pages', 'Sites', 'Apps', 'Portfolios' ] as const;
+export const CATEGORIES = ['IA', 'Landing Pages', 'Sites', 'Apps', 'Portfolios', 'Ecommerce' ] as const;
